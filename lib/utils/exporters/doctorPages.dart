23486acd638296236ccef64170f489647fbaa031
Quách Vinh Quang <hynoras1@gmail.com>
2025-01-07 14:03:31 +0700

    fix a bug where missing closing bracket }

--- lib/utils/exporters/doctorPages.dart
+++ lib/utils/exporters/doctorPages.dart
@@ -6,3 +6,4 @@ export 'package:heart_attack_detection_fe/pages/category/Prescription/Prescripti
 export 'package:heart_attack_detection_fe/pages/category/Prescription/PrescriptionModal/index.dart';
 export 'package:heart_attack_detection_fe/pages/category/Prescription/index.dart';
 export 'package:heart_attack_detection_fe/pages/notFound/notFound.dart';
+export 'package:heart_attack_detection_fe/pages/doctor/PatientRecord/index.dart';
