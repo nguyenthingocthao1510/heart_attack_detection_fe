4ef171327e06a9b4ec622bdd9afbfbcf6dea4065
nguyenthingocthao1510 <153586637+nguyenthingocthao1510@users.noreply.github.com>
2025-01-07 22:34:47 +0700

    Fix feature dashboard

--- lib/services/dashboard.dart
+++ lib/services/dashboard.dart
@@ -8,19 +8,28 @@ class DashboardAPI {
     final response = await dio.get(url);
     final resData = response.data;
 
-    // Check if resData is a list
     if (resData is List) {
-      // Map each item in the list to a Dashboard object
       return resData.map((item) => Dashboard.fromMap(item)).toList();
     } else {
-      // Handle unexpected response structure
+      throw Exception('Unexpected response format: ${resData}');
+    }
+  }
+
+  static Future<List<Dashboard>> getAvgBPM() async {
+    final dio = Dio();
+    final url = 'http://127.0.0.1:5000/api/avg-BPM';
+    final response = await dio.get(url);
+    final resData = response.data;
+    if (resData is List) {
+      return resData.map((item) => Dashboard.fromMap(item)).toList();
+    } else {
       throw Exception('Unexpected response format: ${resData}');
     }
   }
 
   static Future<List<Dashboard>> getTemperature() async {
     final dio = Dio();
-    final url = 'http://127.0.0.1:5000/api/temperature';
+    final url = 'http://127.0.0.1:5000/api/heartbeat';
     final response = await dio.get(url);
     final resData = response.data;
 
